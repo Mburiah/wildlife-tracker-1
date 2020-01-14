7859122a1ac43c5eb00462b9c1c41cb79c65fd1f
Mark Watitwa <markwatitwa98@gmail.com>
2020-01-14 13:10:43 +0300

    db

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.Sql2o;
 
 public class DB {
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-21-101-177.compute-1.amazonaws.com:5432/postgres", "qrfxvjeqixbqsf", "5ca7db3ed82059584bf152e5f8b117be0b490b281abee55cac24a04d3cc1a4d5");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-21-101-177.compute-1.amazonaws.com:5432/dfp0t65hgb3rmt", "qrfxvjeqixbqsf", "5ca7db3ed82059584bf152e5f8b117be0b490b281abee55cac24a04d3cc1a4d5");
 }
