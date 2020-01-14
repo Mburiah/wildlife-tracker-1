0aaf9b2e794c145d5f38d1612ed31e4f0d6ae41c
Mark Watitwa <markwatitwa98@gmail.com>
2020-01-14 13:05:19 +0300

    project functionality

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,12 +3,18 @@ import org.sql2o.Connection;
 import java.util.List;
 
 public class Animal {
+    private int animal_id;
     private String animal_name;
     private int animal_age;
 
-    public Animal(String animal_name, int animal_age) {
+    public Animal(int animal_id,String animal_name, int animal_age) {
+        this.animal_id = animal_id;
         this.animal_name = animal_name;
         this.animal_age = animal_age;
+    }
+
+    public int getAnimal_id() {
+        return animal_id;
     }
 
     public String getAnimal_name() {
@@ -21,7 +27,8 @@ public class Animal {
 
     public void add() {
         try (Connection conn = DB.sql2o.open()) {
-            conn.createQuery("insert into animals(animal_name, animal_age) VALUES(:animal_name, :animal_age)")
+            conn.createQuery("insert into animals(animal_id, animal_name, animal_age) VALUES(:animal_id,:animal_name, :animal_age)")
+                    .addParameter("animal_id", this.animal_id)
                     .addParameter("animal_name", this.animal_name)
                     .addParameter("animal_age", this.animal_age)
                     .executeUpdate();
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,21 +23,37 @@ public class App {
         staticFiles.location("/public");
 
         Map<String, Object> model = new HashMap<>();
-        get("/", (request, response) -> new ModelAndView(model, "index.hbs"), new HandlebarsTemplateEngine());
-
-        get("/animals", (request, response) -> new ModelAndView(model, "animals.hbs"), new HandlebarsTemplateEngine());
-
-        get("/animal/new", (request, response) -> {
+        get("/", (request, response) -> {
             List<Animal> animals = Animal.getAll();
             model.put("animals", animals);
-            return new ModelAndView(model, "animal.hbs");
+            List<Sighting> sightings = Sighting.getAll();
+            model.put("sightings", sightings);
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+        get("/animals", (request, response) -> new ModelAndView(model, "animals.hbs"), new HandlebarsTemplateEngine());
+
+        get("/sightings", (request, response) -> new ModelAndView(model, "sightings.hbs"), new HandlebarsTemplateEngine());
+
+        get("/animal/new", (request, response) -> new ModelAndView(model, "animal.hbs"), new HandlebarsTemplateEngine());
+
+        get("/sightings/new", (request, response) -> new ModelAndView(model, "sighting.hbs"), new HandlebarsTemplateEngine());
 
         post("/animal/add", (request, response) -> {
             String animal_name = request.queryParams("animal_name");
+            int animal_id = Integer.parseInt(request.queryParams("animal_id"));
             int animal_age = Integer.parseInt(request.queryParams("animal_age"));
-            Animal newAnimal = new Animal(animal_name, animal_age);
+            Animal newAnimal = new Animal(animal_id,animal_name, animal_age);
             newAnimal.add();
+            response.redirect("/");
+            return null;
+        });
+
+        post("/sighting/add", (request, response) -> {
+            String animal_name = request.queryParams("animal_name");
+            String location = request.queryParams("location");
+            String ranger_name = request.queryParams("ranger_name");
+            Sighting newSighting = new Sighting(animal_name, location, ranger_name);
+            newSighting.add();
             response.redirect("/");
             return null;
         });
